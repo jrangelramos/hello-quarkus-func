c8f7875cabba1d473cbf2def9462b6f63fe8b35c
Jefferson Ramos <jeramos@redhat.com>
2022-12-19 10:27:17 -0300

    added greeting in 3 languages

--- src/main/java/functions/Function.java
+++ src/main/java/functions/Function.java
@@ -1,23 +1,26 @@
 package functions;
 
 import io.quarkus.funqy.Funq;
+import java.util.*;
 
 /**
  * Your Function class
  */
 public class Function {
 
-    /**
-     * Use the Quarkus Funqy extension for our function. This function simply echoes its input
-     * @param input a Java bean
-     * @return a Java bean
-     */
     @Funq
-    public Output function(Input input) {
+public Output function(Input input) {
+   Map<String, String> greetings = Map.of(
+                   "english", "Welcome",
+                   "spanish", "Bienvenido",
+                   "italian", "Benvenuto");
 
-        // Add business logic here
+   String lang = Optional.ofNullable(input.getLanguage()).orElse("english");
+   String greeting = greetings.getOrDefault(lang, "");
 
-        return new Output(input.getMessage());
-    }
+   return new Output(String.format("%s!!! Here is your message: %s", greeting,
+       input.getMessage()));
+}
+
 
 }
